346a0383e0f769a308acb96f99e60c6052acb092
Mayank Gupta <mayank.pkgupta@outlook.com>
2021-01-23 11:37:52 +0530

    Modified for heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-
+const port = process.env.PORT || 3000
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
 
@@ -85,6 +85,6 @@ app.get('*', (req, res) => {
     res.send("My 404 page")
 })
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000")
+app.listen(port, () => {
+    console.log("Server is up on port " + port)
 })
