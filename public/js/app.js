669ce72f8eb7bfc959f7921c7bf2dbb993f83108
Mayank Gupta <mayank.pkgupta@outlook.com>
2021-01-23 10:33:01 +0530

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+console.log("my script is running !");
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('#locationInput');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    const location = search.value; 
+    url = 'http://localhost:3000/weather?address=' + location
+
+    messageOne.textContent = "Loading ..."
+    messageTwo.textContent = ""
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = 'Error : ' + data.error
+            }
+            else {
+                messageOne.innerHTML = '<B>Location : </B>' + data.location
+                messageTwo.innerHTML = '<b>Forecast : </B>' + data.forecast
+                console.log(data)
+            }
+        }) 
+    })
+})
